33863473e7d8005e8148767173e370b15313f3da
Michael <ac2031@hotmail.com>
2022-04-18 02:03:29 +0200

    :fire:

--- src/components/dom/Login.tsx
+++ src/components/dom/Login.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { register, listenForRegister, listenForErrors } from '../../utils/client';
 
 export default function Login({ setUsername }) {
 	const [queryUsername, setQueryUsername] = useState('');
